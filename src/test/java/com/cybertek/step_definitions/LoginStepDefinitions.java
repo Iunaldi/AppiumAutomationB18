8859afa4ae7ef774eeae105fcc6ef9a8de429634
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-09-15 16:25:02 -0400

    added cucumber

---
+++ src/test/java/com/cybertek/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+    @Given("user click on get started button")
+    public void user_click_on_get_started_button() {
+
+    }
+
+    @When("user logs in with etsy credentials")
+    public void user_logs_in_with_etsy_credentials() {
+
+    }
+
+    @Then("user verifies that etsy logo is displayed")
+    public void user_verifies_that_etsy_logo_is_displayed() {
+
+    }
+}
